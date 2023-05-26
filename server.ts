213a63ccdce0ac50f0ef2ae6f0ea5cc076c0eeaf
Zed <voiddatation@gmail.com>
2023-05-26 15:55:34 +0700

    First Commit

---
+++ server.ts
@@ -0,0 +1,169 @@
+import express, { NextFunction } from "express";
+import cors from "cors";
+import { run, Entry, addEntry, getEntry, removeEntry, updateEntry, getAllEntries, EntryListOptions } from "./services/db";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Fuck You!");
+});
+
+app.get("/testing/:item", (req, res) => {
+	console.log(req.params.item);
+});
+
+app.get("/testing", (req, res) => {
+	console.log(req.query);
+});
+
+
+// Queue Entries from DB
+app.get("/api/item/all", (req, res) => {
+	let limit: number = req.query.limit? Number.parseInt(req.query.limit.toString()): 10;
+	let skip: number = req.query.skip? Number.parseInt(req.query.skip.toString()): 0;
+
+	let options: EntryListOptions = {
+		limit: limit,
+		skip: skip,
+		filter: null
+	};
+
+	getAllEntries(options).then((result) => {
+		console.log(result);
+		res.status(200).send(result);
+	})
+})
+
+// Search Entry from DB
+app.get("/api/item/:id", (req, res) => {
+	if (req.params.id.length != 24) {
+		res.status(400).send(
+			"Length of the ID is incorrect. Must be a 24 characters string."
+		);
+	} else {
+		getEntry(req.params.id).then((result) => {
+			if (result === null) {
+				res.status(204).send(result);
+			} else {
+				res.status(200).send(result);
+			}
+		});
+	}
+});
+
+// Add Entry to DB
+app.put("/api/item", (req, res) => {
+	addEntry(req.body).then((result) => {
+		res.status(201);
+		res.send(result);
+	});
+});
+
+// Remove Entry from DB
+app.delete("/api/item/:id", (req, res) => {
+	if (req.params.id.length != 24) {
+		res.status(400).send(
+			"Length of the ID is incorrect. Must be a 24 characters string."
+		);
+	} else {
+		removeEntry(req.params.id).then((result) => {
+			console.log(result);
+			res.send({message: result});
+		});
+	}
+});
+
+
+// Update Entry in DB
+app.post("/api/item/:id", (req, res) => {
+	if (req.params.id.length != 24) {
+		res.status(400).send(
+			"Length of the ID is incorrect. Must be a 24 characters string."
+		);
+	} else {
+		updateEntry(req.params.id, req.body).then((result) => {
+			console.log(result);
+
+			if (result === null) {
+				res.status(204).send(result);
+			} else {
+				res.status(200).send(result);
+			}
+		});
+	}
+});
+
+app.listen(3000, () => {
+	console.log("Server starting!");
+
+	run().catch((error: string) => {
+		console.log(error);
+		throw "Shit";
+	});
+});
+
+const test: Entry = {
+	name: "test item",
+	description: "test item description",
+	tags: ["for testing purpose", "development"],
+	linkage: [],
+	thumbnail_src: "none",
+	option: [
+		{
+			name: "level of pain",
+			choice: [
+				{
+					label: "level 1",
+					modifier: 0,
+				},
+				{
+					label: "level 2",
+					modifier: 1,
+				},
+				{
+					label: "level 3",
+					modifier: 2,
+				},
+			],
+		},
+		{
+			name: "level of blood",
+			choice: [
+				{
+					label: "level 1",
+					modifier: 0,
+				},
+				{
+					label: "level 2",
+					modifier: 1,
+				},
+				{
+					label: "level 3",
+					modifier: 2,
+				},
+				{
+					label: "level 4",
+					modifier: 3,
+				},
+				{
+					label: "level 5",
+					modifier: 4,
+				},
+			],
+		},
+	],
+	additional: [
+		{
+			name: "control 1",
+			modifier: 1,
+		},
+		{
+			name: "control 2",
+			modifier: 2,
+		},
+	],
+	base_price: 10,
+};
---
+++ services/db.ts
@@ -0,0 +1,153 @@
+import {
+	InsertOneResult,
+	MongoClient,
+	ServerApiVersion,
+	Document,
+	ObjectId,
+	WithId,
+	DeleteResult,
+	UpdateResult,
+	FindCursor,
+} from "mongodb";
+
+const uri: string = process.env.DB_CONN_URL ? process.env.DB_CONN_URL : "mongodb+srv://admin:admin123@glass-edge.hqmedfc.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+export async function run() {
+	try {
+		await client.connect();
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} catch (error) {
+		throw "Fucking server crashed!";
+	}
+}
+
+export function end_server() {
+	client.close();
+}
+
+export type Entry = {
+	name: string;
+	description: string;
+	tags: string[]; // ! Do not let client define this. Only Admin.
+	linkage: string[]; // ! Do not use this yet, not ready for development.
+	thumbnail_src: string;
+	option: EntryOption[];
+	additional: EntryAdditional[];
+	base_price: number;
+};
+
+export type EntryOption = {
+	// * Slider options.
+	name: string;
+	choice: {
+		label: string;
+		modifier: number;
+	}[];
+};
+
+export type EntryAdditional = {
+	// * Boolean options.
+	name: string;
+	modifier: number;
+};
+
+export interface EntryListOptions {
+	limit: number; // Number of entries to be queue
+	skip: number; // Number of entries to be skip
+	filter: any | null; // Filter entries
+}
+
+// * Testing
+export async function getAllEntries(options: EntryListOptions) {
+	return new Promise<Entry[]>(async (resolve, reject) => {
+		const result: FindCursor<WithId<Entry>> = client
+			.db("development")
+			.collection<Entry>("item")
+			.find({}, { limit: options.limit, skip: options.skip });
+
+		let send_back: Entry[] = [];
+		for await (const doc of result) {
+			send_back.push(doc);
+		}
+
+		resolve(send_back);
+	});
+}
+
+// ! DO NOT USE THIS! FOR RESETTING DB ONLY
+export async function dropAllEntries() {
+	return new Promise<any>((resolve, reject) => {
+		client
+			.db("development")
+			.collection<Entry>("item")
+			.deleteMany({})
+			.then((result) => {
+				resolve(result);
+			});
+	});
+}
+
+export async function getEntry(entry_id: string) {
+	return new Promise<WithId<Entry> | null>((resolve, reject) => {
+		client
+			.db("development")
+			.collection<Entry>("item")
+			.findOne({ _id: new ObjectId(entry_id) })
+			.then((result) => {
+				resolve(result);
+			});
+	});
+}
+
+export async function addEntry(entry: Entry) {
+	return new Promise<InsertOneResult<Entry>>((resolve, reject) => {
+		client
+			.db("development")
+			.collection<Entry>("item")
+			.insertOne(entry)
+			.then((result) => {
+				resolve(result);
+			})
+			.catch((failed) => {
+				reject(new Error("Unexpected Failure!"));
+			});
+	});
+}
+
+export async function removeEntry(entry_id: string) {
+	return new Promise<DeleteResult>((resolve, reject) => {
+		client
+			.db("development")
+			.collection<Entry>("item")
+			.deleteOne({ _id: new ObjectId(entry_id) })
+			.then((result) => {
+				resolve(result);
+			});
+	});
+}
+
+export async function updateEntry(entry_id: string, body_replacement: Entry) {
+	return new Promise<UpdateResult<Entry> | null>((resolve, reject) => {
+		client
+			.db("development")
+			.collection<Entry>("item")
+			.updateOne(
+				{ _id: new ObjectId(entry_id) },
+				{ $set: body_replacement }
+			)
+			.then((result) => {
+				resolve(result);
+			});
+	});
+}
